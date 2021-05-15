57c18853b61d5403b9c516d9db2171136f06e8a6
Gerardo Mtz <mtz.juncogerardo@gmail.com>
2021-05-14 21:12:24 -0500

    TAGS CRUD

--- Controllers/TagController.cs
+++ Controllers/TagController.cs
@@ -71,7 +71,7 @@ namespace Shinsekai_API.Controllers
         }
 
         [Authorize]
-        [HttpPost("anime")]
+        [HttpPost("anime/create")]
         public IActionResult SaveAnimeTag([FromBody] AnimeItem anime)
         {
             if (AuthService.AuthorizeAdmin(User.Identity, _context.Users.ToList()))
@@ -89,6 +89,167 @@ namespace Shinsekai_API.Controllers
             return Ok(new OkResponse()
             {
                 Response = "Anime Tag has been saved"
+            });
+        }
+        
+        [Authorize]
+        [HttpPost("line/create")]
+        public IActionResult SaveLineTag([FromBody] LineItem line)
+        {
+            if (AuthService.AuthorizeAdmin(User.Identity, _context.Users.ToList()))
+            {
+                return Unauthorized(new ErrorResponse()
+                {
+                    Error = "You dont have the required role"
+                });
+            }
+
+            line.Id = Guid.NewGuid().ToString();
+            _context.Lines.Add(line);
+            _context.SaveChanges();
+
+            return Ok(new OkResponse()
+            {
+                Response = "Line Tag has been saved"
+            });
+        }
+        
+        [Authorize]
+        [HttpPost("material/create")]
+        public IActionResult SaveMaterialTag([FromBody] MaterialItem material)
+        {
+            if (AuthService.AuthorizeAdmin(User.Identity, _context.Users.ToList()))
+            {
+                return Unauthorized(new ErrorResponse()
+                {
+                    Error = "You dont have the required role"
+                });
+            }
+
+            material.Id = Guid.NewGuid().ToString();
+            _context.Materials.Add(material);
+            _context.SaveChanges();
+
+            return Ok(new OkResponse()
+            {
+                Response = "Material Tag has been saved"
+            });
+        }
+        
+        [Authorize]
+        [HttpDelete("anime/delete")]
+        public IActionResult DeleteAnimeTag([FromQuery] string id)
+        {
+            if (AuthService.AuthorizeAdmin(User.Identity, _context.Users.ToList()))
+            {
+                return Unauthorized(new ErrorResponse()
+                {
+                    Error = "You dont have the required role"
+                });
+            }
+
+            if (id == null)
+            {
+                return BadRequest(new ErrorResponse()
+                {
+                    Error = "No se especifico una anime para eliminar"
+                });
+            }
+
+            var dbAnime = _context.Animes.FirstOrDefault(q => q.Id == id);
+
+            if (dbAnime == null)
+            {
+                return BadRequest(new ErrorResponse()
+                {
+                    Error = "El Articulo que intentas eliminar ya no existe"
+                });
+            }
+
+            _context.Remove(dbAnime);
+            _context.SaveChanges();
+
+            return Ok(new OkResponse()
+            {
+                Response = "La anime se elimino con exito"
+            });
+        }
+        
+        [Authorize]
+        [HttpDelete("line/delete")]
+        public IActionResult DeleteLineTag([FromQuery] string id)
+        {
+            if (AuthService.AuthorizeAdmin(User.Identity, _context.Users.ToList()))
+            {
+                return Unauthorized(new ErrorResponse()
+                {
+                    Error = "You dont have the required role"
+                });
+            }
+
+            if (id == null)
+            {
+                return BadRequest(new ErrorResponse()
+                {
+                    Error = "No se especifico una line para eliminar"
+                });
+            }
+
+            var dbLine = _context.Lines.FirstOrDefault(q => q.Id == id);
+
+            if (dbLine == null)
+            {
+                return BadRequest(new ErrorResponse()
+                {
+                    Error = "El Articulo que intentas eliminar ya no existe"
+                });
+            }
+
+            _context.Remove(dbLine);
+            _context.SaveChanges();
+
+            return Ok(new OkResponse()
+            {
+                Response = "La line se elimino con exito"
+            });
+        }
+
+        [Authorize]
+        [HttpDelete("material/delete")]
+        public IActionResult DeleteMaterialTag([FromQuery] string id)
+        {
+            if (AuthService.AuthorizeAdmin(User.Identity, _context.Users.ToList()))
+            {
+                return Unauthorized(new ErrorResponse()
+                {
+                    Error = "You dont have the required role"
+                });
+            }
+
+            if (id == null)
+            {
+                return BadRequest(new ErrorResponse()
+                {
+                    Error = "No se especifico una material para eliminar"
+                });
+            }
+
+            var dbMaterial = _context.Materials.FirstOrDefault(q => q.Id == id);
+
+            if (dbMaterial == null)
+            {
+                return BadRequest(new ErrorResponse()
+                {
+                    Error = "El Articulo que intentas eliminar ya no existe"
+                });
+            }
+
+            _context.Remove(dbMaterial);
+            _context.SaveChanges();
+
+            return Ok(new OkResponse()
+            {
+                Response = "La material se elimino con exito"
             });
         }
     }
